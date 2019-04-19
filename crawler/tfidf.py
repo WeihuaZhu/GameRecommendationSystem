0ef56d3c4c7e6c74a9095ce40dd9c2b78ad5dd2d
xwsha <shaxiangwei1996@hotmail.com>
2019-04-19 11:11:50 -0400

    output added, please play with it

--- crawler/tfidf.py
+++ crawler/tfidf.py
@@ -1,6 +1,7 @@
 import numpy as np
 import json
 from sklearn.feature_extraction import text
+import heapq
 
 fileName = 'review_all.txt'
 # x = open(fileName).read()
@@ -24,7 +25,11 @@ stop_words = text.ENGLISH_STOP_WORDS.union(gameGeneral).union(platforms).union(n
 tfidf_vec = text.TfidfVectorizer(stop_words=stop_words, min_df=0.05)
 tfidf_matrix = tfidf_vec.fit_transform(reviews).toarray()
 
-d = len(tfidf_vec.vocabulary_)
-
-print(tfidf_matrix.shape)
-print(d)
+#print 10 most important words for each review according to tfidf
+for i in range(10):
+    a = list(tfidf_matrix[i])
+    idx = list(map(a.index, heapq.nlargest(10, a)))
+    for j in idx:
+        print(tfidf_vec.get_feature_names()[j], end = '  ')
+    print()
+    
