b0e196f51e0fbc803f9afb9e21125aa63e9b1b16
Weihua Zhu <zhuweihua2746@gmail.com>
2019-04-20 16:14:22 -0400

    removed "Exit Theatre Mode" in reviews

--- decisiontree/dt.py
+++ decisiontree/dt.py
@@ -10,5 +10,5 @@ clf = tree.DecisionTreeClassifier()
 clf = clf.fit(iris.data, iris.target)
 
 dot_data = tree.export_graphviz(clf, out_file=None) 
-graph = graphviz.Source(dot_data) 
+graph = graphviz.Source(dot_data)
 graph.render("iris")
