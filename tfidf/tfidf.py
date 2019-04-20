16fd253b15c56709044ffa2da60151fc69da6f2b
Chengxi Yao <andy-yao@foxmail.com>
2019-04-19 20:53:40 -0400

    moved tfidf and output to file

--- tfidf/tfidf.py
+++ tfidf/tfidf.py
@@ -2,6 +2,7 @@ import numpy as np
 import json
 from sklearn.feature_extraction import text
 import heapq
+import os
 
 fileName = 'review_all.txt'
 # x = open(fileName).read()
@@ -22,14 +23,17 @@ gameGeneral = {'game','ign','play','playing','player','version','gaming','games'
 noMeaningWords = {'thank','thanks','didn','don','doesn','didn','nearly','hasn','haven','isn'}
 stop_words = text.ENGLISH_STOP_WORDS.union(gameGeneral).union(platforms).union(noMeaningWords)
 # print(stop_words)
-tfidf_vec = text.TfidfVectorizer(stop_words=stop_words, min_df=0.05)
+tfidf_vec = text.TfidfVectorizer(stop_words=stop_words, min_df=0.035)
 tfidf_matrix = tfidf_vec.fit_transform(reviews).toarray()
 
-#print 10 most important words for each review according to tfidf
+print(tfidf_matrix.shape)
+
+#print 10 most important words for each review according to tfidf, first 10 for testing
 for i in range(10):
     a = list(tfidf_matrix[i])
-    idx = list(map(a.index, heapq.nlargest(10, a)))
+    idx = list(map(a.index, heapq.nlargest(80, a)))
+    f = open('tfidf_matrix_test.txt', 'a')
     for j in idx:
-        print(tfidf_vec.get_feature_names()[j], end = '  ')
-    print()
-    
+        print(tfidf_vec.get_feature_names()[j], end = '  ', file = f)
+    print('\n', file = f)
+    f.close()
